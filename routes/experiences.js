e3fea482994da1db35758df9e87d4cc0fda810fd
katungi <dankatdennis@gmail.com>
2021-01-08 05:58:53 +0300

    updated experiences routes

--- routes/experiences.js
+++ routes/experiences.js
@@ -2,11 +2,21 @@ const express = require("express");
 const router = express.Router();
 
 const ExperiencesCtrl = require("../controllers/experience");
-const AuthCtrl = require('../controllers/auth');
+const AuthCtrl = require("../controllers/auth");
 
 router.get("/", ExperiencesCtrl.getExperiences);
-router.get('/secret', AuthCtrl.onlyAuthUser);
-router.get('/:id', ExperiencesCtrl.get)
+router.get("/secret", AuthCtrl.onlyAuthUser);
+router.get("/:id", ExperiencesCtrl.getExperienceById);
+
 router.post("", ExperiencesCtrl.createExperience);
+router.post("/:id/join", AuthCtrl.onlyAuthUser, ExperiencesCtrl.joinExperience);
+router.post(
+  "/:id/leave",
+  AuthCtrl.onlyAuthUser,
+  ExperiencesCtrl.leaveExperience
+);
+
+router.patch("/:id", AuthCtrl.onlyAuthUser, ExperiencesCtrl.updateExperience);
+route.delete("/:id", AuthCtrl.onlyAuthUser, ExperiencesCtrl.deleteExperience);
 
 module.exports = router;
