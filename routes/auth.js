d372ff51319e1b860cc0e4ebd39007c68d99eeb0
katungi <dankatdennis@gmail.com>
2021-01-26 10:24:16 +0300

    trying out passport for auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { signup, signin, signout, requireSignin , googleLogin} = require("../controllers/auth.js");
+const {
+  signup,
+  signin,
+  signout,
+  requireSignin,
+  googleLogin,
+  passportLogin,
+  passportPreLogin
+} = require("../controllers/auth.js");
+const passport = require("passport");
 
 // validators
 const { runValidation } = require("../validators/index");
@@ -11,16 +20,18 @@ const {
 
 router.post("/signup", userSignupValidator, runValidation, signup);
 router.post("/signin", userSigninValidator, runValidation, signin);
-router.get('/signout', signout)
+router.get("/signout", signout);
 
 // google login
-router.post('/google-login', googleLogin);
+router.post("/google-login", googleLogin);
 
-// this is for testing btw
+router.get("/auth/google/redirect", passportPreLogin );
 
-router.get('/secret',requireSignin, (req, res)=> {
-    res.json({
-        message: 'you have access to the secret page dummy!'
-    })
-})
+// this is for testing btw -- remove it from production
+
+router.get("/secret", requireSignin, (req, res) => {
+  res.json({
+    message: "you have access to the secret page dummy!",
+  });
+});
 module.exports = router;
