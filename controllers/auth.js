7c29a8dcc3d11463ba66b837d5d59a83d7cc34e4
katungi <dankatdennis@gmail.com>
2021-01-27 12:04:43 +0300

    fixing bugs

--- controllers/auth.js
+++ controllers/auth.js
@@ -87,59 +87,59 @@ exports.onlyAuthUser = function (req, res, next) {
   return res.status(401).send({ errors: { auth: "Not Authenticated!" } });
 };
 
-const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
-exports.googleLogin = (req, res) => {
-  const idToken = req.body.tokenId;
-  client
-    .verifyIdToken({ idToken, audience: process.env.GOOGLE_CLIENT_ID })
-    .then((response) => {
-      // console.log(response)
-      const { email_verified, name, email, jti } = response.payload;
-      if (email_verified) {
-        User.findOne({ email }).exec((err, user) => {
-          if (user) {
-            // console.log(user)
-            const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
-              expiresIn: "1d",
-            });
-            res.cookie("token", token, { expiresIn: "1d" });
-            const { _id, email, name, role, username } = user;
-            return res.json({
-              token,
-              user: { _id, email, name, role, username },
-            });
-          } else {
-            let username = shortid.generate();
-            let profile = `${process.env.CLIENT_URL}/profile/${username}`;
-            let password = jti;
-            user = new User({ name, email, profile, username, password });
-            user.save((err, data) => {
-              if (err) {
-                return res.status(400).json({
-                  error: errorHandler(err),
-                });
-              }
-              const token = jwt.sign(
-                { _id: data._id },
-                process.env.JWT_SECRET,
-                { expiresIn: "1d" }
-              );
-              res.cookie("token", token, { expiresIn: "1d" });
-              const { _id, email, name, role, username } = data;
-              return res.json({
-                token,
-                user: { _id, email, name, role, username },
-              });
-            });
-          }
-        });
-      } else {
-        return res.status(400).json({
-          error: "Google login failed. Try again.",
-        });
-      }
-    });
-};
+// const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+// exports.googleLogin = (req, res) => {
+//   const idToken = req.body.tokenId;
+//   client
+//     .verifyIdToken({ idToken, audience: process.env.GOOGLE_CLIENT_ID })
+//     .then((response) => {
+//       // console.log(response)
+//       const { email_verified, name, email, jti } = response.payload;
+//       if (email_verified) {
+//         User.findOne({ email }).exec((err, user) => {
+//           if (user) {
+//             // console.log(user)
+//             const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+//               expiresIn: "1d",
+//             });
+//             res.cookie("token", token, { expiresIn: "1d" });
+//             const { _id, email, name, role, username } = user;
+//             return res.json({
+//               token,
+//               user: { _id, email, name, role, username },
+//             });
+//           } else {
+//             let username = shortid.generate();
+//             let profile = `${process.env.CLIENT_URL}/profile/${username}`;
+//             let password = jti;
+//             user = new User({ name, email, profile, username, password });
+//             user.save((err, data) => {
+//               if (err) {
+//                 return res.status(400).json({
+//                   error: errorHandler(err),
+//                 });
+//               }
+//               const token = jwt.sign(
+//                 { _id: data._id },
+//                 process.env.JWT_SECRET,
+//                 { expiresIn: "1d" }
+//               );
+//               res.cookie("token", token, { expiresIn: "1d" });
+//               const { _id, email, name, role, username } = data;
+//               return res.json({
+//                 token,
+//                 user: { _id, email, name, role, username },
+//               });
+//             });
+//           }
+//         });
+//       } else {
+//         return res.status(400).json({
+//           error: "Google login failed. Try again.",
+//         });
+//       }
+//     });
+// };
 
 exports.passportPreLogin = function (req, res, next) {
   passport.authenticate("google", ["profile", "email"]);
